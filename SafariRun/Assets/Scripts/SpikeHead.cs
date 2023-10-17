4470dadee2280416231fa98cc22f5c560421c4e3
TiagoSeixas2103 <tvseixas@gmail.com>
2023-10-17 15:00:34 -0300

    ve se da ruim

---
+++ SafariRun/Assets/Scripts/SpikeHead.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeHead : MonoBehaviour
+{
+    public float minY;
+    public float maxY;
+    private float riseSpeed = 4f;
+    private float fallSpeed = 4f;
+    private Vector2 temp;
+    public bool isBottom;
+    public List<GameObject> players;           
+
+    void Update(){
+        if (isBottom) {
+            temp.x = transform.position.x;
+            temp.y = maxY;
+            transform.position = Vector2.MoveTowards(transform.position, temp, Time.deltaTime * riseSpeed);
+            if (transform.position.y == maxY) {
+                isBottom = false;
+            }
+        }
+        else if (!isBottom) {
+            temp.x = transform.position.x;
+            temp.y = minY;
+            transform.position = Vector2.MoveTowards(transform.position, temp, Time.deltaTime * fallSpeed);
+            if (transform.position.y == minY) {
+                isBottom = true;
+            }
+        }
+    }
+}
