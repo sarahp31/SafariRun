cde76cfdd2fe138585490a34b447070039e216dc
Sarah <72346043+sarahp31@users.noreply.github.com>
2023-10-19 09:24:08 -0300

    musica de coleta

--- SafariRun/Assets/Scripts/CollectFruit.cs
+++ SafariRun/Assets/Scripts/CollectFruit.cs
@@ -6,11 +6,18 @@ public class CollectFruit : MonoBehaviour
 {
     public GameObject Player;
     public Animator animator;
+    public AudioClip soundColetar;
+    private AudioSource audioSource;
+
+    void Start(){
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter2D(Collider2D collider) {
         if (collider.gameObject.name == Player.name){
             animator.SetTrigger("collect");
             StartCoroutine(timer());
+            audioSource.PlayOneShot(soundColetar);
         }
     } 
 
