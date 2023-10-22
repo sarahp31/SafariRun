8f3803b31f0bd8fa19aaf5970caf603effbb32fa
J <joras.custodio@gmail.com>
2023-10-22 18:18:10 -0300

    itch.io 1.o version

--- SafariRun/Assets/Scripts/PlayerDeath.cs
+++ SafariRun/Assets/Scripts/PlayerDeath.cs
@@ -9,19 +9,30 @@ public class PlayerDeath : MonoBehaviour
     public GameObject player1;
     public Animator animator2;
     public GameObject player2;
+    public GameObject deathText;
+
+    void Start() {
+        deathText.SetActive(false);
+    }
 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.name == "Player_1" || collision.gameObject.name == "Player_2"){
             animator1.SetTrigger("death");
             animator2.SetTrigger("death");
-            StartCoroutine(timer());
+            StartCoroutine(timer0());
         }
     } 
 
-    IEnumerator timer() {
+    IEnumerator timer0() {
         yield return new WaitForSeconds(0.4f);
         player1.SetActive(false);
         player2.SetActive(false);
-        SceneManager.LoadScene(2);
+        deathText.SetActive(true);
+        StartCoroutine(timer1());
+    }
+
+    IEnumerator timer1() {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
